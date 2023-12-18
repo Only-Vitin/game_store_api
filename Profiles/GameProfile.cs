37ab98894bd0ddb1c4c11f6dccf1e5bd1be63145
Only-Vitin <vitinhosantossilva123@gmail.com>
2023-12-18 17:34:03 -0300

    creating user DTOs

--- Profiles/GameProfile.cs
+++ Profiles/GameProfile.cs
@@ -1,14 +1,15 @@
 using AutoMapper;
+using game_store_api.Dto;
 using game_store_api.Models;
 
 namespace web_api.Profiles
 {
-   public class GameProfile : Profile
-   {
-    public GameProfile()
+    public class GameProfile : Profile
     {
-        CreateMap<GameDto, Game>();
-        CreateMap<Game, GameDto>();
-    }
-   } 
+        public GameProfile()
+        {
+            CreateMap<GameDto, Game>();
+            CreateMap<Game, GameDto>();
+        }
+    } 
 }
--- Profiles/UserProfile.cs
+++ Profiles/UserProfile.cs
@@ -1,13 +1,16 @@
 using AutoMapper;
+using game_store_api.Dto;
 using game_store_api.Models;
 
 namespace web_api.Profiles
 {
    public class UserProfile : Profile
    {
-        public UserProfile()
-        {
-        
-        }
+      public UserProfile()
+      {
+         CreateMap<PostUserDto, User>();
+         CreateMap<User, GetUserDto>();
+         CreateMap<User, GetUserByIdDto>();
+      }
    }
 }
